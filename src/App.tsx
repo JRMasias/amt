86c80c83c350a5e01ee34ec0bdbc5b0eb145cebe
J. R. Masias <131043700+JRMasias@users.noreply.github.com>
2026-09-05 00:03:17 -0500

    Create Navbar (#1)
    * Creating blank menu placeholders
    
    * Navbar menu prototype
    
    * Finalize Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from './components/navigation/Navbar';
 function App() {
 
   return (
-    <main className="bg-neutral-950 h-screen">
+    <main className="h-screen max-w-300 m-auto antialiased tracking-wide">
       <Navbar />
     </main>
   );
---
+++ src/components/navigation/Desktop.tsx
@@ -0,0 +1,10 @@
+export default function Desktop() {
+     return (
+          <ul className="flex gap-4 text-2xl font-semibold">
+               <li className="hover:text-[#ffaa00] cursor-pointer duration-300 ease-in-out">Home</li>
+               <li className="hover:text-[#ffaa00] cursor-pointer duration-300 ease-in-out">Movies</li>
+               <li className="hover:text-[#ffaa00] cursor-pointer duration-300 ease-in-out">Order</li>
+               <li className="hover:text-[#ffaa00] cursor-pointer duration-300 ease-in-out">Contact</li>
+          </ul>
+     );
+}
