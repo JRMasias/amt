4b57d584df7b1980544254b106cd26dcadb39aa1
Johnathon R. Masias <john.r.masias@gmail.com>
2026-09-05 11:01:04 -0500

    Make navbar sticky

--- src/components/navigation/MobileMenu.tsx
+++ src/components/navigation/MobileMenu.tsx
@@ -18,7 +18,7 @@ export default function MobileMenu() {
                     { !isOpen ? <FaBars size={ 30 } color="ffaa00" /> : <FaTimes size={ 30 } color="ffaa00" /> }
                </button>
 
-               <ul id="mobile-menu" className={ clsx("absolute left-0 w-full mt-16 bg-neutral-900/30 text-[#ffaa00] font-bold text-2xl shadow-lg flex flex-col items-center gap-2 py-4 transition-all duration-500 ease-in-out", isOpen ? "top-0 " : "-top-70") }>
+               <ul id="mobile-menu" className={ clsx("absolute left-0 w-full mt-16 bg-neutral-900/30 text-[#ffaa00] font-bold text-2xl shadow-lg flex flex-col items-center gap-2 py-4 transition-all duration-500 ease-in-out", isOpen ? "top-0 " : "-top-100") }>
                     <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">Home</li>
                     <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">Movies</li>
                     <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">Order</li>
