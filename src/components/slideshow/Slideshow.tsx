89ae9f371736bb8af91563886470f38cd0cf5c2b
Johnathon R. Masias <john.r.masias@gmail.com>
2026-09-06 20:06:22 -0500

    Finish Slideshow Placeholder

--- src/components/slideshow/Slideshow.tsx
+++ src/components/slideshow/Slideshow.tsx
@@ -1,9 +1,24 @@
+import { useState, useEffect } from "react";
 import { movies } from "../../utils/movies";
 
+
 export default function Slideshow() {
+     const [ screenSize, setScreenSize ] = useState(window.innerWidth);
+
+     useEffect(() => {
+          const handleResize = () => {
+               setScreenSize(window.innerWidth);
+          };
+          window.addEventListener('resize', handleResize);
+          return () => {
+               window.removeEventListener('resize', handleResize);
+          };
+     }, []);
+
      return (
           <section>
-               <img src={ movies[ 0 ].image } alt={ movies[ 0 ].title } className="h-40 w-full aspect-video" />
+               { screenSize > 768 ? <img src={ movies[ 0 ].image } alt={ movies[ 0 ].title } className="w-full" /> :
+                    <img src={ movies[ 0 ].imageMobile } alt={ movies[ 0 ].title } className="w-full max-h-screen" /> }
           </section>
      );
 }
--- src/utils/movies.tsx
+++ src/utils/movies.tsx
@@ -1,14 +1,25 @@
 import movie1 from "../assets/movie_1.jpg";
+import movie_1_mobile from "../assets/movie_1_mobile.png";
 import movie2 from "../assets/movie_2.jpg";
+import movie_2_mobile from "../assets/movie_2_mobile.png";
 import movie3 from "../assets/movie_3.jpg";
+import movie_3_mobile from "../assets/movie_3_mobile.png";
 import movie4 from "../assets/movie_4.jpg";
+import movie_4_mobile from "../assets/movie_1_mobile.png";
 import movie5 from "../assets/movie_5.jpg";
+import movie_5_mobile from "../assets/movie_1_mobile.png";
 import movie6 from "../assets/movie_6.jpg";
+import movie_6_mobile from "../assets/movie_1_mobile.png";
 import movie7 from "../assets/movie_7.jpg";
+import movie_7_mobile from "../assets/movie_1_mobile.png";
 import movie8 from "../assets/movie_8.jpg";
+import movie_8_mobile from "../assets/movie_1_mobile.png";
 import movie9 from "../assets/movie_9.jpg";
+import movie_9_mobile from "../assets/movie_1_mobile.png";
 import movie10 from "../assets/movie_10.jpg";
+import movie_10_mobile from "../assets/movie_1_mobile.png";
 import movie11 from "../assets/movie_11.jpg";
+import movie_11_mobile from "../assets/movie_1_mobile.png";
 
 export const movies = [
      {
@@ -16,6 +27,7 @@ export const movies = [
           title: "Super Action Movie",
           description: "An action-packed adventure that will keep you on the edge of your seat.",
           image: movie1,
+          imageMobile: movie_1_mobile,
           rating: 4.5,
      },
      {
@@ -23,6 +35,7 @@ export const movies = [
           title: "Afraid",
           description: "A psychological thriller that will keep you guessing until the very end.",
           image: movie2,
+          imageMobile: movie_2_mobile,
           rating: 4.8,
      },
      {
@@ -30,6 +43,7 @@ export const movies = [
           title: "Serious Film",
           description: "A thought-provoking drama that will leave you reflecting long after the credits roll.",
           image: movie3,
+          imageMobile: movie_3_mobile,
           rating: 4.3,
      },
      {
@@ -37,6 +51,7 @@ export const movies = [
           title: "The Princess Elf",
           description: "A magical fantasy tale that will transport you to a world of wonder and enchantment.",
           image: movie4,
+          imageMobile: movie_4_mobile,
           rating: 4.9,
      },
      {
@@ -44,6 +59,7 @@ export const movies = [
           title: "Dark of Space",
           description: "A sci-fi epic that will take you on a thrilling journey through the cosmos.",
           image: movie5,
+          imageMobile: movie_5_mobile,
           rating: 4.1,
      },
      {
@@ -51,6 +67,7 @@ export const movies = [
           title: "Super Hero Movie: The Moviefilm",
           description: "A superhero adventure that will keep you on the edge of your seat.",
           image: movie6,
+          imageMobile: movie_6_mobile,
           rating: 3.9,
      },
      {
@@ -58,6 +75,7 @@ export const movies = [
           title: "My Funny Tragedy",
           description: "A comedic tragedy that will make you laugh and cry at the same time.",
           image: movie7,
+          imageMobile: movie_7_mobile,
           rating: 3.8,
      },
      {
@@ -65,6 +83,7 @@ export const movies = [
           title: "New Love Again 2: Electric Boogaloo",
           description: "A romantic comedy that will warm your heart and make you believe in love again.",
           image: movie8,
+          imageMobile: movie_8_mobile,
           rating: 4.0,
      },
      {
@@ -72,6 +91,7 @@ export const movies = [
           title: "Action Fight IV",
           description: "An action-packed adventure that will keep you on the edge of your seat.",
           image: movie9,
+          imageMobile: movie_9_mobile,
           rating: 4.0,
      },
      {
@@ -79,6 +99,7 @@ export const movies = [
           title: "History Movie",
           description: "A historical drama that will transport you back in time and immerse you in the past.",
           image: movie10,
+          imageMobile: movie_10_mobile,
           rating: 2.7,
      },
      {
@@ -86,6 +107,7 @@ export const movies = [
           title: "Still Terrified",
           description: "A horror movie that will keep you on the edge of your seat and make you jump out of your skin.",
           image: movie11,
+          imageMobile: movie_11_mobile,
           rating: 4.9,
      },
 
