1f3b4a480c168eda7a3dc56405f0b45cb4cc11b6
Johnathon R. Masias <john.r.masias@gmail.com>
2026-09-06 20:49:52 -0500

    Fix overflow in animated banner

--- src/components/Banner/AnimatedBanner.tsx
+++ src/components/Banner/AnimatedBanner.tsx
@@ -3,7 +3,7 @@ import "./animatedBanner.css";
 export default function AnimatedBanner() {
      return (
           <div className="bg-[#FFAA00]">
-               <p className="font-bold py-2 text-right text-xl animated-banner">Order ahead and skip the line!!!</p>
+               <p className="font-bold py-2 text-right text-xl animated-banner overflow-hidden">Order ahead and skip the line!!!</p>
           </div>
      );
 }
