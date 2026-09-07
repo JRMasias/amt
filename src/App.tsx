e1ee7783d793f62f73f4f23a0b179d728894a738
J. R. Masias <131043700+JRMasias@users.noreply.github.com>
2026-09-06 20:23:25 -0500

    Slideshow (#3)
    * Slideshow Placeholder
    
    * Finish Slideshow Placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/navigation/Navbar';
+import Slideshow from './components/slideshow/Slideshow';
 
 function App() {
 
   return (
     <main className="h-screen max-w-300 m-auto antialiased tracking-wide">
       <Navbar />
+      <Slideshow />
     </main>
   );
 }
