e63724f658df6630376e9840a34470c4d43c00cf
J. R. Masias <131043700+JRMasias@users.noreply.github.com>
2026-09-08 16:03:41 -0500

    Footer (#11)
    * Initialize footer
    
    * Initialize footer

--- src/components/ui/Divider.tsx
+++ src/components/ui/Divider.tsx
@@ -7,13 +7,20 @@ export default function Divider({ children }: DividerProps) {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+=======
+>>>>>>> 9a729a2 (Footer (#11))
+<<<<<<< HEAD
           <div className="w-full border-t border-neutral-800 mt-10 py-5 flex justify-evenly items-center">
 =======
           <div className="w-full border-t border-b border-neutral-800 my-10 py-5 flex justify-evenly items-center">
+<<<<<<< HEAD
 >>>>>>> 8a41aae (Initialize footer)
 =======
           <div className="w-full border-t border-neutral-800 mt-10 py-5 flex justify-evenly items-center">
 >>>>>>> 25e2ed2 (Fix spacing issues)
+=======
+>>>>>>> 6abb1da (Footer (#11))
+>>>>>>> 9a729a2 (Footer (#11))
                { children }
           </div>
      );
