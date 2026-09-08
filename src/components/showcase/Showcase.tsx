02fcc95fa9dd7035d6f345e6eaeb4f4bbd292f5d
J. R. Masias <131043700+JRMasias@users.noreply.github.com>
2026-09-08 16:34:46 -0500

    Make desktop full screen width (#14)

--- src/components/showcase/Showcase.tsx
+++ src/components/showcase/Showcase.tsx
@@ -7,7 +7,7 @@ export default function Showcase() {
                { movies.filter(movie => movie.showcaseImage).map((movie) => (
                     <div className="flex justify-between items-start bg-linear-to-br from-neutral-800/40 to-neutral-950">
                          <div className={ clsx("relative w-3/5 flex justify-between items-center", movie.id % 2 === 0 ? "order-first" : "order-last") }>
-                              <img src={ movie.showcaseImage } alt="Showcase" className="object-cover" />
+                              <img src={ movie.showcaseImage } alt="Showcase" />
                               <div className="bg-linear-to-l from-transparent to-neutral-950 absolute top-0 left-0 h-full w-full"></div>
                          </div>
                          <div className="w-2/5 text-white p-10">
