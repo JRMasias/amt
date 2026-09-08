6bd5d56c516dd4dfe7526c245fda945c87e843e4
J. R. Masias <john.r.masias@gmail.com>
2026-09-08 16:03:11 -0500

    Initialize footer

--- src/components/ui/Divider.tsx
+++ src/components/ui/Divider.tsx
@@ -5,7 +5,11 @@ interface DividerProps {
 export default function Divider({ children }: DividerProps) {
      return (
 
+<<<<<<< HEAD
           <div className="w-full border-t border-neutral-800 mt-10 py-5 flex justify-evenly items-center">
+=======
+          <div className="w-full border-t border-b border-neutral-800 my-10 py-5 flex justify-evenly items-center">
+>>>>>>> 8a41aae (Initialize footer)
                { children }
           </div>
      );
