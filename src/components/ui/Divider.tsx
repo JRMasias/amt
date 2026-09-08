152245fc059dfe2cd3cc241ed20d2573c8b2dfb8
J. R. Masias <john.r.masias@gmail.com>
2026-09-08 16:06:18 -0500

    Fix spacing issues

--- src/components/ui/Divider.tsx
+++ src/components/ui/Divider.tsx
@@ -6,10 +6,14 @@ export default function Divider({ children }: DividerProps) {
      return (
 
 <<<<<<< HEAD
+<<<<<<< HEAD
           <div className="w-full border-t border-neutral-800 mt-10 py-5 flex justify-evenly items-center">
 =======
           <div className="w-full border-t border-b border-neutral-800 my-10 py-5 flex justify-evenly items-center">
 >>>>>>> 8a41aae (Initialize footer)
+=======
+          <div className="w-full border-t border-neutral-800 mt-10 py-5 flex justify-evenly items-center">
+>>>>>>> 25e2ed2 (Fix spacing issues)
                { children }
           </div>
      );
