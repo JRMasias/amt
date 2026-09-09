e29e131d85e9bbb12196026946d0faa8011c15f6
J. R. Masias <john.r.masias@gmail.com>
2026-09-09 15:05:28 -0500

    Add footer menu

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import AMC from "../assets/7.png";
 import amcLogo from "../assets/9.png";
+import { links } from "../utils/footerlinks";
 import Divider from "./ui/Divider";
 import YellowButton from "./ui/YellowButton";
 import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
@@ -18,43 +19,17 @@ export default function Footer() {
                     <FaTwitter className="text-2xl w-10 h-10 p-2 rounded-full border border-white text-white cursor-pointer hover:border-[#FFAA00] hover:text-[#FFAA00] hover:scale-105 transition-all duration-300" />
                     <FaYoutube className="text-2xl w-10 h-10 p-2 rounded-full border border-white text-white cursor-pointer hover:border-[#FFAA00] hover:text-[#FFAA00] hover:scale-105 transition-all duration-300" />
                </Divider>
-               <div className="w-full bg-neutral-800 p-5 flex justify-between items-center">
-                    <div>
-                         <h1 className="text-white text-2xl font-bold mb-2">
-                              About Us
-                         </h1>
-                         <ul className="text-white text-lg">
-                              <li>Item 1</li>
-                              <li>Item 2</li>
-                         </ul>
-                    </div>
-                    <div>
-                         <h1 className="text-white text-2xl font-bold mb-2">
-                              Movies
-                         </h1>
-                         <ul className="text-white text-lg">
-                              <li>Item 1</li>
-                              <li>Item 2</li>
-                         </ul>
-                    </div>
-                    <div>
-                         <h1 className="text-white text-2xl font-bold mb-2">
-                              Programming
-                         </h1>
-                         <ul className="text-white text-lg">
-                              <li>Item 1</li>
-                              <li>Item 2</li>
-                         </ul>
-                    </div>
-                    <div>
-                         <h1 className="text-white text-2xl font-bold mb-2">
-                              More
-                         </h1>
-                         <ul className="text-white text-lg">
-                              <li>Item 1</li>
-                              <li>Item 2</li>
-                         </ul>
-                    </div>
+               <div className="w-full bg-neutral-800 p-10 flex justify-between items-start">
+                    { links.map(link => {
+                         return <div className="flex flex-col items-start w-full">
+                              { link.map(item => {
+                                   return <ul className="text-white flex flex-col justify-start items-center">
+                                        { item.category ? <h1 className="text-2xl font-bold mb-2">{ item.title }</h1> :
+                                             <li className="text-white text-lg hover:text-[#FFAA00] cursor-pointer transition-colors duration-300">{ item.title }</li> }
+                                   </ul>;
+                              }) }
+                         </div>;
+                    }) }
                </div>
                <div className="w-full flex flex-col justify-center items-center border-t border-neutral-600 py-5 bg-neutral-800 gap-4">
                     <img src={ amcLogo } alt="AMC Logo" className="w-24" />
